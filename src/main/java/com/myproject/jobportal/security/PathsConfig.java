3d3baf19eb5f0162c4cd5c7c924868bbe32545a3
Sachin Sharma <codwork.tdotslash@gmail.com>
2026-06-29 00:53:34 +0530

    add: added new file for paths separation for METHOD 3: URL-Based Authorization (or Request-Level Access Control)

---
+++ src/main/java/com/myproject/jobportal/security/PathsConfig.java
@@ -0,0 +1,22 @@
+package com.myproject.jobportal.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class PathsConfig {
+
+    public static final String [] PUBLIC_PATHS = {"/api/companies/public",
+            "/api/swagger-ui.html",
+            "/swagger-ui/**",
+            "/api/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/webjars/**"};
+
+
+    public static final String [] SECURED_PATHS ={"/api/**"};
+
+}
